a2229fb704edd9425e0f198a061a551a6196a3b5
software1398 <scorpio1398@hanmail.net>
2022-01-03 00:41:14 +0900

    MemberRepository

---
+++ src/main/java/com/example/demomember/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.demomember;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(String id);
+    Optional<Member> findByName(String name);
+    Optional<Member> findByAge(String age);
+    List<Member> findAll();
+}
---
+++ src/main/java/com/example/demomember/MemoryMemberRepository.java
@@ -0,0 +1,41 @@
+package com.example.demomember;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static final Map<String, Member> store = new HashMap<>();
+
+    @Override
+    public Member save(Member member) {
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(String id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> Objects.equals(member.getName(), name))
+                .findAny();
+    }
+
+    @Override
+    public Optional<Member> findByAge(String age) {
+        return store.values().stream()
+                .filter(member -> Objects.equals(member.getAge(), age))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
